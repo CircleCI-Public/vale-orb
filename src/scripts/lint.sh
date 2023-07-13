87fc13ea6fc76dd7353824f2442eeb1b912e897d
KyleTryon <Kyle.Tryon@circleci.com>
2023-07-13 13:12:19 -0400

    feat: add error for no config

--- src/scripts/lint.sh
+++ src/scripts/lint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+redtext() {
+  echo -e "\033[0;31m$1\033[0m"
+}
+
+if [[ ! -f "$ORB_STR_CLI_CONFIG" ]]; then
+  redtext "No configuration file found at $ORB_STR_CLI_CONFIG"
+  echo "To get started, you'll need a configuration file (.vale.ini)"
+  exit 1
+fi
+
 vale --glob "$ORB_STR_CLI_GLOB" --config "$ORB_STR_CLI_CONFIG"
