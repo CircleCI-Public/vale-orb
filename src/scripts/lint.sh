3c2000489ab43410b5ec98cc5b83395143dc295f
KyleTryon <Kyle.Tryon@circleci.com>
2023-07-13 15:07:01 -0400

    chore: remove debug

--- src/scripts/lint.sh
+++ src/scripts/lint.sh
@@ -4,11 +4,6 @@ redtext() {
   echo -e "\033[0;31m$1\033[0m"
 }
 
-# debug
-echo "Glob: $ORB_STR_CLI_GLOB"
-echo "Config: $ORB_STR_CLI_CONFIG"
-ls -lah
-
 if [[ ! -f "$ORB_STR_CLI_CONFIG" ]]; then
   redtext "No configuration file found at $ORB_STR_CLI_CONFIG"
   echo "To get started, you'll need a configuration file (.vale.ini)"
