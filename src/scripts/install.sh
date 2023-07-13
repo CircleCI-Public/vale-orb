582f4730f5e4f87bd819bc1ba63c645040594e4d
KyleTryon <Kyle.Tryon@circleci.com>
2023-07-12 20:33:14 -0400

    feat: sanitize version input

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -9,6 +9,9 @@ if [[ $ORB_STR_CLI_VERSION == "latest" ]]; then
   export ORB_STR_CLI_VERSION
 fi
 
+# sanitize version
+ORB_STR_CLI_VERSION=${ORB_STR_CLI_VERSION//v}
+
 curl -sSL "https://github.com/errata-ai/vale/releases/download/v${ORB_STR_CLI_VERSION}/vale_${ORB_STR_CLI_VERSION}_Linux_64-bit.tar.gz" -o vale.tar.gz
 tar -xzf vale.tar.gz
 $SUDO mv vale /usr/local/bin
