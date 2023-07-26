a13ede3f4bce3b06b1b851d91573d31cf6565377
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-07-26 19:00:03 -0300

    feat: allow users to run vale only for modified files

--- src/scripts/lint.sh
+++ src/scripts/lint.sh
@@ -4,8 +4,19 @@ redtext() {
   echo -e "\033[0;31m$1\033[0m"
 }
 
+# $1: glob
+# $2: config
+# $3: base_dir or files
+sync_and_run_vale() {
+  set -x
+  vale sync
+  vale --glob="$1" --config="$2" "$3"
+  set +x
+}
+
 VALE_EVAL_CLI_CONFIG="$(eval echo "$VALE_EVAL_CLI_CONFIG")"
 VALE_STR_CLI_GLOB="$(circleci env subst "$VALE_STR_CLI_GLOB")"
+VALE_STR_REFERENCE_BRANCH="$(circleci env subst "$VALE_STR_REFERENCE_BRANCH")"
 
 VALE_EVAL_CLI_BASE_DIR="$(eval echo "$VALE_EVAL_CLI_BASE_DIR")"
 VALE_EVAL_CLI_BASE_DIR="${VALE_EVAL_CLI_BASE_DIR/\~/$HOME}"
@@ -21,7 +32,16 @@ if [[ ! -f "$VALE_EVAL_CLI_CONFIG" ]]; then
   '
   exit 1
 fi
-set -x
-vale sync
-vale --glob="$VALE_STR_CLI_GLOB" --config="$VALE_EVAL_CLI_CONFIG" "$VALE_EVAL_CLI_BASE_DIR"
-set +x
+
+if [[ "$VALE_ENUM_STRATEGY" == "all" ]]; then
+  sync_and_run_vale "$VALE_STR_CLI_GLOB" "$VALE_EVAL_CLI_CONFIG" "$VALE_EVAL_CLI_BASE_DIR"
+elif [[ "$VALE_ENUM_STRATEGY" == "modified" ]]; then
+  echo "Checking for modified files..."
+  modified_files="$(git diff --name-only "$VALE_STR_REFERENCE_BRANCH")"
+  [ -z "$modified_files" ] && { echo "No modified files found"; exit 0; }
+  files_space_delimited="$(echo "$modified_files" | tr '\n' ' ')"
+  sync_and_run_vale "$VALE_STR_CLI_GLOB" "$VALE_EVAL_CLI_CONFIG" "$files_space_delimited"
+else
+  echo "Invalid strategy: $VALE_ENUM_STRATEGY"
+  exit 1
+fi
