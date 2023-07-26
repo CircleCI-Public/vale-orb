02ff7e57d04ac08cf96736091e446bcdb0db7cde
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-07-26 19:18:01 -0300

    fix: lint issues

--- src/scripts/lint.sh
+++ src/scripts/lint.sh
@@ -6,11 +6,12 @@ redtext() {
 
 # $1: glob
 # $2: config
-# $3: base_dir or files
+# $3: files
 sync_and_run_vale() {
   set -x
   vale sync
-  vale --glob="$1" --config="$2" "$3"
+  # shellcheck disable=SC2086
+  vale --glob="$1" --config="$2" $3  # No quotes around $3
   set +x
 }
 
@@ -21,7 +22,7 @@ VALE_STR_REFERENCE_BRANCH="$(circleci env subst "$VALE_STR_REFERENCE_BRANCH")"
 VALE_EVAL_CLI_BASE_DIR="$(eval echo "$VALE_EVAL_CLI_BASE_DIR")"
 VALE_EVAL_CLI_BASE_DIR="${VALE_EVAL_CLI_BASE_DIR/\~/$HOME}"
 
-if [[ ! -f "$VALE_EVAL_CLI_CONFIG" ]]; then
+if [ ! -f "$VALE_EVAL_CLI_CONFIG" ]; then
   redtext "No configuration file found at $VALE_EVAL_CLI_CONFIG"
   echo "To get started, you'll need a configuration file (.vale.ini)"
   echo "Create a config file, or modify the 'config' parameter for this job"
@@ -33,18 +34,19 @@ if [[ ! -f "$VALE_EVAL_CLI_CONFIG" ]]; then
   exit 1
 fi
 
-if [[ "$VALE_ENUM_STRATEGY" == "all" ]]; then
+if [ "$VALE_ENUM_STRATEGY" = "all" ]; then
   sync_and_run_vale "$VALE_STR_CLI_GLOB" "$VALE_EVAL_CLI_CONFIG" "$VALE_EVAL_CLI_BASE_DIR"
-elif [[ "$VALE_ENUM_STRATEGY" == "modified" ]]; then
+elif [ "$VALE_ENUM_STRATEGY" = "modified" ]; then
   echo "Installing git..."
-  command -v git &>/dev/null || { apk add git; }
+  command -v git > /dev/null 2>&1 || { apk add git; }
 
   echo "Checking for modified files..."
   modified_files="$(git diff --name-only "$VALE_STR_REFERENCE_BRANCH")"
-  files_space_delimited="$(echo "$modified_files" | tr '\n' ' ')"
+  echo "$modified_files"
 
+  modified_files_space_separated=$(echo "$modified_files" | tr '\n' ' ')
   echo "Running vale on modified files..."
-  sync_and_run_vale "$VALE_STR_CLI_GLOB" "$VALE_EVAL_CLI_CONFIG" "$files_space_delimited"
+  sync_and_run_vale "$VALE_STR_CLI_GLOB" "$VALE_EVAL_CLI_CONFIG" "$modified_files_space_separated"
 else
   echo "Invalid strategy: $VALE_ENUM_STRATEGY"
   exit 1
