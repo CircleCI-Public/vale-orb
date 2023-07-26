7df71e9e5fe0ab42be968a2884623d880284be19
Eric Ribeiro <ericribeiro@outlook.com.br>
2023-07-26 20:06:43 -0300

    feat: check only modified files (#7)

--- src/scripts/lint.sh
+++ src/scripts/lint.sh
@@ -4,13 +4,25 @@ redtext() {
   echo -e "\033[0;31m$1\033[0m"
 }
 
+# $1: glob
+# $2: config
+# $3: files
+sync_and_run_vale() {
+  set -x
+  vale sync
+  # shellcheck disable=SC2086
+  vale --glob="$1" --config="$2" $3  # No quotes around $3
+  set +x
+}
+
 VALE_EVAL_CLI_CONFIG="$(eval echo "$VALE_EVAL_CLI_CONFIG")"
 VALE_STR_CLI_GLOB="$(circleci env subst "$VALE_STR_CLI_GLOB")"
+VALE_STR_REFERENCE_BRANCH="$(circleci env subst "$VALE_STR_REFERENCE_BRANCH")"
 
 VALE_EVAL_CLI_BASE_DIR="$(eval echo "$VALE_EVAL_CLI_BASE_DIR")"
 VALE_EVAL_CLI_BASE_DIR="${VALE_EVAL_CLI_BASE_DIR/\~/$HOME}"
 
-if [[ ! -f "$VALE_EVAL_CLI_CONFIG" ]]; then
+if [ ! -f "$VALE_EVAL_CLI_CONFIG" ]; then
   redtext "No configuration file found at $VALE_EVAL_CLI_CONFIG"
   echo "To get started, you'll need a configuration file (.vale.ini)"
   echo "Create a config file, or modify the 'config' parameter for this job"
@@ -21,7 +33,21 @@ if [[ ! -f "$VALE_EVAL_CLI_CONFIG" ]]; then
   '
   exit 1
 fi
-set -x
-vale sync
-vale --glob="$VALE_STR_CLI_GLOB" --config="$VALE_EVAL_CLI_CONFIG" "$VALE_EVAL_CLI_BASE_DIR"
-set +x
+
+if [ "$VALE_ENUM_STRATEGY" = "all" ]; then
+  sync_and_run_vale "$VALE_STR_CLI_GLOB" "$VALE_EVAL_CLI_CONFIG" "$VALE_EVAL_CLI_BASE_DIR"
+elif [ "$VALE_ENUM_STRATEGY" = "modified" ]; then
+  echo "Installing git..."
+  command -v git > /dev/null 2>&1 || { apk add git; }
+
+  echo "Checking for modified files..."
+  modified_files="$(git diff --name-only "$VALE_STR_REFERENCE_BRANCH")"
+  echo "$modified_files"
+
+  modified_files_space_separated=$(echo "$modified_files" | tr '\n' ' ')
+  echo "Running vale on modified files..."
+  sync_and_run_vale "$VALE_STR_CLI_GLOB" "$VALE_EVAL_CLI_CONFIG" "$modified_files_space_separated"
+else
+  echo "Invalid strategy: $VALE_ENUM_STRATEGY"
+  exit 1
+fi
